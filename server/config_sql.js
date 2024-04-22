bc567928fc7c73df209609efaafb16b9df254f10
Harshada Borse <harshadaborse9029@gmail.com>
2024-04-22 23:54:40 +0530

    passward updated

--- server/config_sql.js
+++ server/config_sql.js
@@ -1,7 +1,7 @@
 const config = {
   host: process.env.DB_HOST || "localhost",
   user: process.env.DB_USER || "root",
-  password: process.env.DB_PASSWORD || "karan4342",
+  password: process.env.DB_PASSWORD || "Harshada615*",
   database: "apartment",
   insecureAuth: true,
 };
